adb1dbc5085a6f9a5b55fe53a0b15dbe4e304000
Myles Lake <lakemyles@gmail.com>
2022-02-08 16:31:54 -0600

    Add section 22 notes on js arrow functions and array methods

---
+++ notes/beginner-javascript/array-methods.js
@@ -0,0 +1,143 @@
+// Array Methods
+
+// forEach method ---------------------------------------------------
+// Accepts a callback function
+// Calls the function once per element in the array
+// More commonly used before js added 'for of' loop
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach(function (num) {
+    console.log(num * num);
+});
+
+// Can pass in named function, but less common
+function print (num) {
+    console.log(num * num);
+}
+numbers.forEach(print);
+
+// Newer way of doing same thing
+// More common recently
+for(let num of numbers) {
+    console.log(num * num);
+}
+
+// map Method ---------------------------------------------------
+// Creates a new array with the results of calling a callback
+// on every element in the array
+// Doesn't mutate original array
+// Example: Creates a new array with each number double the
+//          value in the original array
+const doubles = numbers.map(function(num) {
+    return num * 2;
+});
+
+// Same thing written as an arrow function
+const doubles = numbers.map((num) => {
+    return num * 2;
+});
+
+// Same thing written as a one line arrow function
+// with an implicit return
+const doubles = numbers.map(num => num * 2);
+
+// setTimeout and setInterval functions ---------------------
+// Not array methods, but take a callback param
+
+// setTimeout
+// Takes a callback function and an interval in ms
+// Code after this will still be executed and this won't 
+// be called until 3s later (async?)
+// Example: 
+setTimeout(() => {
+    console.log("...are you still there?")
+}, 3000);
+
+// setInterval
+// Similar to timeout, but reoccurs every interval until
+// the interval is cleared
+const id = setInterval(() => {
+    console.log(Math.random())
+}, 3000);
+
+clearInterval(id);
+
+// filter Method -------------------------------------------------
+// Creates a new array with all elements that pass the test 
+// implemented by the provided function
+// Callback function needs to be a boolean function
+// If callback returns true, the array item is added to the new 
+// filtered array
+// Example: 
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const odds = nums.filter(n => {
+    return n % 2 == 1;
+});
+
+// Same thing with one liner and implicit return
+// This example ends up like .Where in C#
+const odds = nums.filter(n => n % 2 == 1);
+
+// Combining Methods
+// Can indent the chained together methods similar to how we
+// do LINQ in C#
+const squaredOdds = nums.filter(n => n % 2 == 1).map(n => n * n);
+
+// some Method ----------------------------------------------------
+// Returns true if ANY of the array elements pass the test function
+// C# .Any / .Contains
+// Example: 
+const hasTwo = nums.some(n => n == 2);
+
+// every Method
+// Returns true if ALL of the array elements pass the test function
+// C# .All
+// Example: 
+const hasOnlyOddNumbers = nums.every(n => n % 2 == 1);
+
+// reduce Method -------------------------------------------------
+// Executes a reducer function on each element of the array
+// resulting in a single value
+// Example: Summing all items in an array
+// accumulator represents the sum
+// Whatever is returned will be used as next iterations accumulator
+// currentValue represents the current value in the array being
+// iterated over
+const numbers = [3, 5, 7, 9, 11];
+let sum = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+});
+
+// Example: Same concept, but using implicit return
+// to shorten syntax
+const prices = [9.99, 4.95, 3.99, 4.00];
+let totalPrice = prices.reduce((total, price) => total + price);
+
+// Example: Find lowest value in array
+let minPrice = prices.reduce((min, price) => {
+    if (price < min) {
+        return price;
+    } 
+    return min;
+})
+
+// Example: Working with objects
+movies = [
+    {
+        title: "Bourne Identity",
+        rating: 100,
+        year: 2002
+    }
+];
+
+bestMovie = movies.reduce((highestRatedMovie, currentMovie) => {
+    if(currentMovie.rating > highestRatedMovie.rating) {
+        return currentMovie.rating;
+    }
+    return highestRatedMovie.rating;
+});
+
+
+
+
+
---
+++ notes/beginner-javascript/arrow-functions.js
@@ -0,0 +1,62 @@
+// Arrow Functions
+// Syntactically compact alternative to a regular
+// function expression
+// Omit 'function' keyword before parameters and add
+// => (the arrow) after parameters
+// Parentheses are options with only one parameter
+// Example:
+const square = x => {
+    return x * x;
+};
+
+const sum = (x, y) => {
+    return x + y;
+};
+
+// Original way of writing the same thing
+const square = function(x) {
+    return x * x;
+};
+
+const sum = function(x, y) {
+    return x + y;
+};
+
+// No params syntax
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
+};
+
+// Syntax for implicit return
+// No return keyword required
+// Replace curly brackets with parentheses
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1 // no semicolon?
+);
+
+// One line syntax with implicit return
+const sum = (x, y) => x + y\
+
+// 'this' Keyword
+// Inside an arrow function, 'this' keyword refers to the scope
+// that it was created in. In this instance that is the Window object
+const person = {
+    firstName: 'Viggo',
+    lastName: 'Mortensen',
+    fullName: function () {
+        return `${this.firstName} ${this.lastName}` 
+    },
+    badFullName: () => {
+        return `${this.firstName} ${this.lastName}` // Error: 'this' undefined
+    },
+    shoutName: function () {
+        setTimeout(function() { // setTimeout is a method on Window object
+            console.log(this.fullName()) // Error: this refers to Window
+        }, 3000)
+    },
+    yellName: () => {           // Works
+        setTimeout(function() { // Now keyword 'this' is the same as it is
+            console.log(this.fullName()) // for the nested function
+        }, 3000)
+    }
+};
