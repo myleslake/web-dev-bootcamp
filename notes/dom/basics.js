1516e1449e6eb414fab2f2b23e79562a20c961a5
Myles Lake <lakemyles@gmail.com>
2022-02-11 14:18:52 -0600

    Update section 24 notes on the DOM

--- notes/dom/basics.js
+++ notes/dom/basics.js
@@ -4,7 +4,7 @@
 // When browser loads a webpage it creates javascript
 // objects based on the html and css
 
-// Selecting Elements
+// Selecting Elements --------------------------------------------------------------------
 // Basically can use any query you could with css
 const myElement0 = document.querySelector('#id');           // returns first match
 const myElements1 = document.querySelectorAll('img');       // returns HTML
@@ -32,14 +32,14 @@ for (let img of myElements5) {
 // getAttribute     // setAttribute()  // these use the text of the html not an object
 // remove()
 
-// Attributes
+// Attributes ----------------------------------------------------------------------------
 // Can access basically any HTML attribute
 // Can use this or the methods
 myElements1[0].id
 myElements1[0].src
 myElements1[0].title
 
-// Styles
+// Styles --------------------------------------------------------------------------------
 // Adding/removing class instead of in-line styles
 // is considered best practice
 // The keys are camelCase instead of dash-case
@@ -49,10 +49,41 @@ const h1 = document.querySelector("h1");
 h1.style.color = "green";
 h1.style.fontSize = "14px";
 
-// Gets the actual computed style including style sheets
-window.getComputedStyle(h1).fontSize 
+// Gets the actual computed style including external style sheets
+window.getComputedStyle(h1).fontSize
 
-// Classes
+// Classes -------------------------------------------------------------------------------
+h1.classList.add("d-none");
+h1.classList.toggle("d-none");
+h1.classList.remove("d-none");
+h1.classList.contains("d-none");    // returns boolean
+
+// Relative properties -------------------------------------------------------------------
+h1.parentElement
+h1.children             // HTML collection
+h1.nextElementSibling           // .nextSibling gives text nodes instead of elements
+h1.previousElementSibling       // can include whitespace and new lines
+
+// Add/Remove Elements -------------------------------------------------------------------
+
+// Add
+const img = document.createElement('img');
+img.src = 'https://google.com/someimage.jpg';
+document.body.appendChild(img);
+
+const h3 = document.createElement('h3');
+h3.innerText = "NEW H3!!";
+document.body.appendChild(h3);
+// or
+const p = document.querySelector('p');
+p.append(h3); // just appends it to the end // can append multiple items // No IE support
+p.prepend(h3); // adds to beginning
+p.insertAdjacentElement('beforebegin', h3);
+
+// Remove
+p.removeChild(h3);
+p.remove();         // Removes element that calls it    // No IE support
+
 
 
 
