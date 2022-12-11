d71e55b18f6662547f40df9b17bc6d498ad3d907
Myles Lake <lakemyles@gmail.com>
2022-12-11 11:24:50 -0600

    Add view and logic for comments/index

--- practice/35-restful-routing/index.js
+++ practice/35-restful-routing/index.js
@@ -10,9 +10,28 @@ app.use(express.json());                            // To parse json in incoming
 app.set('views', path.join(__dirname, 'views'));    // Set directory of views for our project
 app.set('view engine', 'ejs');                      // Set view engine for templating to ejs
 
+// Fake database
+let comments = [
+    {
+        id: 1,
+        username: "Todd Downing",
+        comment: "I am a terrible offensive coordinator!"
+    },
+    {
+        id: 2,
+        username: "Jon Robinson",
+        comment: "I sure wish I didn't trade AJ Brown!"
+    }
+];
+
 // Home
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+// Index
+app.get("/comments", (req, res) => {
+    res.render("comments/index", { comments });
 });
 
 // Get
