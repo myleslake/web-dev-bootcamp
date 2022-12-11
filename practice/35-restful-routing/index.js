d1872570885321ad3bd222383e007b9657162a4e
Myles Lake <lakemyles@gmail.com>
2022-12-11 12:05:47 -0600

    Add new route and view for editing comments

--- practice/35-restful-routing/index.js
+++ practice/35-restful-routing/index.js
@@ -1,11 +1,13 @@
 // Imports
 const path = require('path');
+const methodOverride = require('method-override')
 const express = require('express');
 const app = express();
 
 // Middleware
 app.use(express.urlencoded({extended: true }));     // To Parse form data in incoming request body
 app.use(express.json());                            // To parse json in incoming request body
+app.use(methodOverride('_method'))
 
 app.set('views', path.join(__dirname, 'views'));    // Set directory of views for our project
 app.set('view engine', 'ejs');                      // Set view engine for templating to ejs
@@ -24,18 +26,45 @@ let comments = [
     }
 ];
 
-// Home
+// Home - GET - Basic index home page - NOT part of restful routing pattern
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-// Index
+// Index - GET - Renders multiple resources
 app.get("/comments", (req, res) => {
     res.render("comments/index", { comments });
 });
 
-// Get
+// Show/Details - GET - Renders details about a single resource
+app.get("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    res.render("comments/details", { comment });
+});
 
+// Edit - GET - Renders a form to edit a resource
+app.get("/comments/:id/edit", (req, res) => {
+    //const id = req.params.id;
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    res.render("comments/edit", { comment });
+});
+
+// Update - PATCH - Updates an existing resource
+app.patch("/comments/:id", (req, res) => {
+
+});
+
+// New - GET - Renders a form to create a new resource
+app.get("/comments/new", (req, res) => {
+    res.render("comments/new");
+});
+
+// Create - POST - Creates a new resource
+app.get("/comments", (req, res) => {
+    res.render("comments/new");
+});
 
 // Post
 
@@ -49,7 +78,7 @@ app.get("/comments", (req, res) => {
 // Delete
 
 
-// Default
+// Default - GET - NOT part of restful routing pattern
 app.get('*', (req, res) => {
     res.send("Oops! Page not found!");
 });
@@ -58,3 +87,25 @@ app.get('*', (req, res) => {
 app.listen(3000, (error) => {
     console.log("LISTENING ON PORT 3000!");
 });
+
+
+// Notes
+// restful routing maps to basic CRUD operations
+
+// syntax: VERB /resource
+// Because we have different verbs, we can keep a uniform path name
+// Resource is the name of the resource, but plural
+// GET /comments
+// POST /comments
+// GET /comments/:id
+// PATCH /comments/:id
+// DELETE /comments/:id
+
+// Route naming convention/pattern - Started in Ruby on Rails
+// Index route - show all/multiple resources
+// New route - renders form to create new resource
+// Create route - create new resource
+// Show route - show one resource (details route)
+// Edit route - render form to update one resource\
+// Update route - update one resource
+// Destroy route - delete one resource
