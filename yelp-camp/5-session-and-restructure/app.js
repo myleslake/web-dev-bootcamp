6ff57e6b481d42d1e51e2059e0fa2ee38a50613a
Myles Lake <lakemyles@gmail.com>
2023-03-23 21:09:30 -0500

    add lecture notes on authentication and bcrypt

--- yelp-camp/5-session-and-restructure/app.js
+++ yelp-camp/5-session-and-restructure/app.js
@@ -11,7 +11,7 @@ const methodOverride = require('method-override');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -71,7 +71,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
+app.listen(3030, () => {
     console.log('Serving on port 3000')
 })
 
